6ffc6b0576f89f7fa2bbe32f48568e9af26d532f
Igor Ponso <igorponso07@gmail.com>
2022-12-18 22:16:48 -0300

    build: initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <article>
+    <h1>Genius</h1>
+  </article>
+</template>
+
+<style scoped></style>
