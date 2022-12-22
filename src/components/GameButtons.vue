fa6b722a293e6c330f6c2341a7af753ab690dde4
Igor Ponso <igorponso07@gmail.com>
2022-12-21 21:45:55 -0300

    feat: add functions to make game work
    addValue, clearPlayerArray, clearGameArray, addRandomArrayPosition

--- src/components/GameButtons.vue
+++ src/components/GameButtons.vue
@@ -2,21 +2,43 @@
 import { reactive } from "vue";
 
 const gameArray: number[] = reactive([]);
+const playerArray: number[] = reactive([]);
 
 const addValue = (buttonValue: number) => {
-  gameArray.push(buttonValue);
+  playerArray.push(buttonValue);
   console.log(gameArray);
+};
+
+const clearplayerArray = () => {
+  playerArray.length = 0;
+};
+
+const clearGameArray = () => {
+  gameArray.length = 0;
+};
+
+const addRandomArrayPosition = () => {
+  gameArray.push(Math.floor(Math.random() * 4));
 };
 </script>
 
 <template>
-  <article>
-    <button @click="addValue(1)">RED</button>
-    <button @click="addValue(2)">GREEN</button>
-    <button @click="addValue(3)">BLUE</button>
-    <button @click="addValue(4)">YELLOW</button>
-  </article>
   {{ gameArray }}
+  {{ playerArray }}
+  <article id="genius-buttons">
+    <button @click="addValue(0)">RED</button>
+    <button @click="addValue(1)">GREEN</button>
+    <button @click="addValue(2)">BLUE</button>
+    <button @click="addValue(3)">YELLOW</button>
+  </article>
+  <button @click="clearplayerArray(), clearGameArray()">Clear</button>
+  <button @click="addRandomArrayPosition">Random</button>
 </template>
 
-<style scoped lang="stylus"></style>
+<style scoped lang="stylus">
+
+#genius-buttons
+  display flex
+  gap 1.5rem
+
+</style>
