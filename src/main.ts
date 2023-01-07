749d8e1c53c627e94e02e426d5b4576b104fc28f
Igor Ponso <igorponso07@gmail.com>
2023-01-07 03:33:57 -0300

    style: change styl file location + change css

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,29 @@
-import App from "./App.vue";
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import { createI18n } from "vue-i18n";
+import App from './App.vue';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { createI18n } from 'vue-i18n';
+import { OhVueIcon, addIcons } from 'oh-vue-icons';
+import { BiLinkedin } from 'oh-vue-icons/icons';
+import ptBR from './locales/pt_br.json';
 
-import ptBR from "./locales/pt_br.json";
+import '@/style.css';
+import '@/assets/stylus/__colors.styl';
+import '@/assets/stylus/__spacing.styl';
 
-import "@/style.css";
-import "@/assets/stylus/__colors.styl";
+addIcons(BiLinkedin);
 
 const i18n = createI18n({
   legacy: true,
   allowComposition: true,
-  locale: "pt-br",
+  locale: 'pt-br',
   messages: {
-    "pt-br": ptBR,
+    'pt-br': ptBR,
   },
 });
 const pinia = createPinia();
 const app = createApp(App);
 
+app.component('v-icon', OhVueIcon);
 app.use(i18n);
 app.use(pinia);
-app.mount("#app");
+app.mount('#app');
